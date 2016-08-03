369c74a6b8aa3b990e298ca1623c3921c789bd90
Nada Amin <namin@alum.mit.edu>
2016-08-03 10:40:56 +0200

    baby steps towards fig1, starting with time.elm from tutorial

---
+++ vehicle.elm
@@ -0,0 +1,64 @@
+import Html exposing (Html)
+import Html.App as App
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time exposing (Time, second)
+
+
+
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model = Time
+
+
+init : (Model, Cmd Msg)
+init =
+  (0, Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      (newTime, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every second Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    svg [ viewBox "0 0 100 100", width "300px" ]
+      [ rect [ x "10", y "10", width "50", height "30", fill "#ffff00", stroke "#00ffff" ] []
+      , rect [ x "5", y "18", width "20", height "12", fill "#ff00ff" ] []
+      , line [ x1 "5", y1 "25", x2 "70", y2 "25", stroke "#023963" ] []
+      ]
